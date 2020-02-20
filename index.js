898026f2b687007c42fb7d9b99c08518698cf10a
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-02-20 23:19:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -132,14 +132,13 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
-
    e.preventDefault();
    e.stopPropagation();
 
    document.addEventListener('keydown', function(e) {
      if (e.which === LEFT_ARROW){
        moveDodgerLeft();
-     } else if (e.which === RIGHT_ARROW) {
+     } if (e.which === RIGHT_ARROW) {
        moveDodgerRight();
      }
 });
